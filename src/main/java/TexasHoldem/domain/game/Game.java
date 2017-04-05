ac07f386d344fe3298e388d61bc77f74214cf702
RonenB <butirevr@post.bgu.ac.il>
2017-04-05 19:16:37 +0300

    Added Game class, and corresponding GameSettings Class. Some basic fields, and methods for beginning.

--- src/main/java/TexasHoldem/domain/game/Game.java
+++ src/main/java/TexasHoldem/domain/game/Game.java
@@ -1,7 +1,49 @@
 package TexasHoldem.domain.game;
 
+import TexasHoldem.domain.users.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Created by Tal on 05/04/2017.
+ * Created by RonenB on 05/04/2017.
  */
 public class Game {
+    private GameSettings settings;
+    private int id;
+    private List<Player> players;
+    private List<Round> rounds;
+    private List<User> spectators;
+
+    public Game(GameSettings settings, User creator){
+        this.rounds=new ArrayList<Round>();
+        this.players=new ArrayList<Player>(); // TODO: add a new constructed associated player (to user) to the players list.
+        this.settings=settings;
+        if(settings.isAcceptSpectating()) spectators=new ArrayList<User>();
+        else spectators=null;
+    }
+
+    public boolean joinGame(User user,boolean spectate){
+        if(spectate){
+            if(spectators == null) return false; // TODO: exception maybe? cant spectate becuase game canot be spectated
+            else spectators.add(user);
+        }
+        else if (isFull()) return false;//Todo : game is full exception maybe?
+        return true;
+    }
+
+    public boolean startNewRound(){
+        if(players.size()< settings.getPlayerRange().getLeft())
+            return false; // TODO: sure ? exception maybe?
+        else{
+            Round rnd=new Round();
+            rounds.add(rnd);
+            rnd.start();
+            return true; // TODO: no need, change signautre maybe to void and throw exception in the first condition leg.
+        }
+    }
+
+    private boolean isFull(){
+        return players.size() == settings.getPlayerRange().getRight();
+    }
 }
--- src/main/java/TexasHoldem/domain/game/Round.java
+++ src/main/java/TexasHoldem/domain/game/Round.java
@@ -5,4 +5,6 @@ package TexasHoldem.domain.game;
  */
 public class Round {
 
+    public void start(){}
+
 }
