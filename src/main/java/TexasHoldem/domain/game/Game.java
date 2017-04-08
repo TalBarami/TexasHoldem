f511ca0494636c1ee03464c51829387558aa2856
RonenB <butirevr@post.bgu.ac.il>
2017-04-08 11:50:54 +0300

    - new Exceptions - Some changes

--- src/main/java/TexasHoldem/domain/game/Game.java
+++ src/main/java/TexasHoldem/domain/game/Game.java
@@ -1,5 +1,9 @@
 package TexasHoldem.domain.game;
 
+import TexasHoldem.common.Exceptions.BelowBuyInPolicyException;
+import TexasHoldem.common.Exceptions.CantSpeactateThisRoomException;
+import TexasHoldem.common.Exceptions.GameIsFullException;
+import TexasHoldem.common.Exceptions.NoMinimumAmountOfPlayersException;
 import TexasHoldem.domain.users.User;
 
 import java.util.ArrayList;
@@ -23,23 +27,30 @@ public class Game {
         else spectators=null;
     }
 
-    public boolean joinGame(User user,boolean spectate){
+    public boolean joinGame(User user,boolean spectate,int buyIn) throws CantSpeactateThisRoomException,
+            GameIsFullException, BelowBuyInPolicyException {
         if(spectate){
-            if(spectators == null) return false; // TODO: exception maybe? cant spectate becuase game canot be spectated
+            if(spectators == null)
+                throw new CantSpeactateThisRoomException("Selected game can't be spectated due to it's settings.");
             else spectators.add(user);
         }
-        else if (isFull()) return false;//Todo : game is full exception maybe?
+        else if (isFull())
+            throw new GameIsFullException("Can't join game as player because it's full.");
+        else if(buyIn<settings.getBuyInPolicy())
+            throw new BelowBuyInPolicyException("Minimal buy-in is " + settings.getBuyInPolicy() + " ");
+        // todo: maybe there is need to save he Game for User? mapping between players and buy-in for each ?
+        players.add(new Player(user));
         return true;
     }
 
-    public boolean startNewRound(){
+    public void startNewRound() throws NoMinimumAmountOfPlayersException {
         if(players.size()< settings.getPlayerRange().getLeft())
-            return false; // TODO: sure ? exception maybe?
+            throw new NoMinimumAmountOfPlayersException("Can't start round, minimal amount for a new round is "+
+                    settings.getPlayerRange().getLeft()+", but currently only "+players.size() +" exist.");
         else{
             Round rnd=new Round();
             rounds.add(rnd);
             rnd.start();
-            return true; // TODO: no need, change signautre maybe to void and throw exception in the first condition leg.
         }
     }
 
--- src/main/java/TexasHoldem/domain/game/Player.java
+++ src/main/java/TexasHoldem/domain/game/Player.java
@@ -1,7 +1,14 @@
 package TexasHoldem.domain.game;
 
+import TexasHoldem.domain.users.User;
+
 /**
  * Created by Tal on 05/04/2017.
  */
 public class Player {
+    private User user;
+    public Player(User user){
+        this.user=user;
+    }
+
 }
