bef5c0f8c1160f203b12585da8108c46cc9fed59
RotemWald <waldr@post.bgu.ac.il>
2017-04-12 20:08:04 +0300

    Round class reverted to the original version. Start using mocks for testing.

--- src/test/java/TexasHoldem/domain/game/RoundTest.java
+++ src/test/java/TexasHoldem/domain/game/RoundTest.java
@@ -15,15 +15,15 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 /**
  * Created by RotemWald on 11/04/2017.
  */
 public class RoundTest {
     Round round1;
-
-    public RoundTest() {
-        setRound1();
-    }
 
     @Before
     public void setUp() throws Exception {
@@ -58,6 +58,40 @@ public class RoundTest {
     }
 
     @Test
+    public void round1_testRunTurnRound() {
+        User user1 = new User("waldr", "1234", "waldr@post.bgu.ac.il", LocalDate.now(), null);
+        User user2 = new User("hodbub", "1234", "hobdud@post.bgu.ac.il", LocalDate.now(), null);
+        User user3 = new User("achiadg", "1234", "achiadg@post.bgu.ac.il", LocalDate.now(), null);
+        User user4 = new User("ronenb", "1234", "ronenb@post.bgu.ac.il", LocalDate.now(), null);
+
+        Player player1 = mock(Player.class);
+        Player player2 = mock(Player.class);
+        Player player3 = mock(Player.class);
+        Player player4 = mock(Player.class);
+
+        List<Player> playerList1 = new LinkedList<Player>();
+        playerList1.add(player1);
+        playerList1.add(player2);
+        playerList1.add(player3);
+        playerList1.add(player4);
+
+        GameSettings settings1 = new GameSettings("Game1", GameSettings.GamePolicy.NOLIMIT, 100, 10, 5, 100, 2, 9, false);
+        Game game1 = new Game(settings1, user1, new LeagueManager());
+        Round round1 = new Round(playerList1, settings1, 0);
+
+        when(player1.chooseAction(any())).thenReturn(GameActions.CHECK);
+        when(player2.chooseAction(any())).thenReturn(GameActions.CHECK);
+        when(player3.chooseAction(any())).thenReturn(GameActions.CHECK);
+        when(player4.chooseAction(any())).thenReturn(GameActions.CHECK);
+
+        round1.playFlopOrTurnRound();
+
+        Assert.assertTrue(round1.getPotAmount() == 0);
+        Assert.assertTrue(round1.getActivePlayers().size() == 4);
+    }
+
+    /*
+    @Test
     public void round1_testDealCards() throws Exception {
         round1.runDealCards();
 
@@ -66,6 +100,7 @@ public class RoundTest {
         }
     }
 
+    @Test
     public void round1_testPaySmallAndBigBlind() throws Exception {
         int smallBlindAmount = 5;
         int bigBlindAmount = 10;
@@ -75,12 +110,14 @@ public class RoundTest {
         int smallPlayerOldChipAmount = smallPlayer.getChipsAmount();
         int bigPlayerOldChipAmount = bigPlayer.getChipsAmount();
 
+        round1.runDealCards();
         round1.runPaySmallAndBigBlind();
 
         Assert.assertTrue(smallPlayer.getChipsAmount() == smallPlayerOldChipAmount - smallBlindAmount);
         Assert.assertTrue(bigPlayer.getChipsAmount() == bigPlayerOldChipAmount - bigBlindAmount);
     }
 
+    @Test
     public void round1_testPlayPreFlopRound() throws Exception {
         Map<Player, List<Pair<GameActions, Integer>>> playerDecisions = new HashMap<Player, List<Pair<GameActions, Integer>>>();
 
@@ -109,6 +146,8 @@ public class RoundTest {
         playerDecisions.put(bigPlayer, bigPlayerDecisionList);
         playerDecisions.put(lastPlayer, lastPlayerDecisionList);
 
+        round1.runDealCards();
+        round1.runPaySmallAndBigBlind();
         round1.runPlayPreFlopRound(playerDecisions);
 
         Assert.assertTrue(round1.getPotAmount() == 40);
@@ -121,7 +160,8 @@ public class RoundTest {
         Assert.assertTrue(lastPlayer.getUser().getBalance() == lastPlayerOldRealCurrency);
     }
 
-    public void round1_testPlayFlopRound() {
+    @Test
+    public void round1_testPlayTurnOrFlopRound() {
         Map<Player, List<Pair<GameActions, Integer>>> playerDecisions = new HashMap<Player, List<Pair<GameActions, Integer>>>();
 
         Player dealer = round1.getCurrentDealerPlayer();
@@ -138,6 +178,25 @@ public class RoundTest {
         List<Pair<GameActions, Integer>> smallPlayerDecisionList = new LinkedList<Pair<GameActions, Integer>>();
         List<Pair<GameActions, Integer>> bigPlayerDecisionList = new LinkedList<Pair<GameActions, Integer>>();
         List<Pair<GameActions, Integer>> lastPlayerDecisionList = new LinkedList<Pair<GameActions, Integer>>();
+
+        dealerDecisionList.add(Pair.of(GameActions.CALL, null));
+        smallPlayerDecisionList.add(Pair.of(GameActions.CALL, null));
+        bigPlayerDecisionList.add(Pair.of(GameActions.CHECK, null));
+        lastPlayerDecisionList.add(Pair.of(GameActions.CALL, null));
+
+        playerDecisions.put(dealer, dealerDecisionList);
+        playerDecisions.put(smallPlayer, smallPlayerDecisionList);
+        playerDecisions.put(bigPlayer, bigPlayerDecisionList);
+        playerDecisions.put(lastPlayer, lastPlayerDecisionList);
+
+        round1.runDealCards();
+        round1.runPaySmallAndBigBlind();
+        round1.runPlayPreFlopRound(playerDecisions);
+
+        dealerDecisionList = new LinkedList<Pair<GameActions, Integer>>();
+        smallPlayerDecisionList = new LinkedList<Pair<GameActions, Integer>>();
+        bigPlayerDecisionList = new LinkedList<Pair<GameActions, Integer>>();
+        lastPlayerDecisionList = new LinkedList<Pair<GameActions, Integer>>();
 
         dealerDecisionList.add(Pair.of(GameActions.CALL, null));
         smallPlayerDecisionList.add(Pair.of(GameActions.RAISE, 10));
@@ -164,44 +223,5 @@ public class RoundTest {
         Assert.assertTrue(bigPlayer.getUser().getBalance() == bigPlayerOldRealCurrency);
         Assert.assertTrue(lastPlayer.getUser().getBalance() == lastPlayerOldRealCurrency);
     }
-
-    public void round1_runPlayRiverRound() {
-        Map<Player, List<Pair<GameActions, Integer>>> playerDecisions = new HashMap<Player, List<Pair<GameActions, Integer>>>();
-
-        Player dealer = round1.getCurrentDealerPlayer();
-        Player smallPlayer = round1.getSmallPlayer();
-        Player bigPlayer = round1.getBigPlayer();
-
-        double dealerOldRealCurrency = dealer.getUser().getBalance();
-        double smallPlayerOldRealCurrency = smallPlayer.getUser().getBalance();
-        double bigPlayerOldRealCurrency = bigPlayer.getUser().getBalance();
-
-        List<Pair<GameActions, Integer>> dealerDecisionList = new LinkedList<Pair<GameActions, Integer>>();
-        List<Pair<GameActions, Integer>> smallPlayerDecisionList = new LinkedList<Pair<GameActions, Integer>>();
-        List<Pair<GameActions, Integer>> bigPlayerDecisionList = new LinkedList<Pair<GameActions, Integer>>();
-
-        smallPlayerDecisionList.add(Pair.of(GameActions.CHECK, null));
-        bigPlayerDecisionList.add(Pair.of(GameActions.RAISE, 10));
-        dealerDecisionList.add(Pair.of(GameActions.RAISE, 20));
-        smallPlayerDecisionList.add(Pair.of(GameActions.FOLD, null));
-        bigPlayerDecisionList.add(Pair.of(GameActions.RAISE, 30));
-        dealerDecisionList.add(Pair.of(GameActions.CALL, null));
-
-        playerDecisions.put(dealer, dealerDecisionList);
-        playerDecisions.put(smallPlayer, smallPlayerDecisionList);
-        playerDecisions.put(bigPlayer, bigPlayerDecisionList);
-
-        round1.runPreStartOfNewTurn();
-        round1.runPlayRiverRound(playerDecisions);
-
-        Assert.assertTrue(round1.getPotAmount() == 220);
-        Assert.assertTrue(round1.getActivePlayers().size() == 2);
-        Assert.assertTrue(dealer.getChipsAmount() == 20);
-        Assert.assertTrue(smallPlayer.getChipsAmount() == 50);
-        Assert.assertTrue(bigPlayer.getChipsAmount() == 20);
-
-        Assert.assertTrue(dealer.getUser().getBalance() == dealerOldRealCurrency);
-        Assert.assertTrue(smallPlayer.getUser().getBalance() == smallPlayerOldRealCurrency);
-        Assert.assertTrue(bigPlayer.getUser().getBalance() == bigPlayerOldRealCurrency);
-    }
+    */
 }
