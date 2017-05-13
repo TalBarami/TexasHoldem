77772fa0ef1bb1ab0e358c531cda224e327eb371
RotemWald <waldr@post.bgu.ac.il>
2017-05-13 15:24:19 +0300

    Added SessionController and GameController in Communication layer. Added new client entities. Added server<->client objects converters. AppConfig updated with support of other services. GamePolicy changed to save values to types.

--- src/main/java/TexasHoldem/domain/game/GamePolicy.java
+++ src/main/java/TexasHoldem/domain/game/GamePolicy.java
@@ -4,5 +4,14 @@ package TexasHoldem.domain.game;
  * Created by Tal on 19/04/2017.
  */
 public enum GamePolicy {
-    LIMIT, NOLIMIT, POTLIMIT
+    LIMIT(0), NOLIMIT(1), POTLIMIT(2);
+
+    private int policy;
+    GamePolicy(int policy) {
+        this.policy = policy;
+    }
+
+    public int getPolicy() {
+        return policy;
+    }
 }
