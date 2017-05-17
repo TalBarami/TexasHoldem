f3f6b5433b44d0d0f87d1a6d74d54881e0305691
TalBarami <talbaramii@gmail.com>
2017-05-17 15:17:34 +0300

    GUI: Deposit bug fixed.

--- src/main/java/TexasHoldem/domain/user/Transaction.java
+++ src/main/java/TexasHoldem/domain/user/Transaction.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
  * Created by rotemwald on 16/05/17.
  */
 
-@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+@JsonFormat(shape = JsonFormat.Shape.STRING)
 public enum Transaction {
     DEPOSIT, WITHDRAW
 }
