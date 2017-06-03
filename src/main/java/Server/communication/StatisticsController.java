5633bf4c86fea5201aac696ba7d2f55519b5d8cf
RonenB <butirevr@post.bgu.ac.il>
2017-06-03 16:54:30 +0300

    Implementation of communication layer support of getting: Gross profit Games played Highest cash gain

--- src/main/java/Server/communication/StatisticsController.java
+++ src/main/java/Server/communication/StatisticsController.java
@@ -4,11 +4,14 @@ import Exceptions.EntityDoesNotExistsException;
 import Exceptions.InvalidArgumentException;
 import MutualJsonObjects.ResponseMessage;
 import Server.service.StatisticsService;
+import javafx.util.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
@@ -28,13 +31,30 @@ public class StatisticsController {
     @RequestMapping(method = GET, value="/statistics/{username}/avgNeto")
     public ResponseMessage getUserAvgNetoProfit(@PathVariable("username") String userName) throws EntityDoesNotExistsException, InvalidArgumentException {
         double avgNeto=service.getUserAvgNetoProfit(userName);
-        return new ResponseMessage<>("Average cash gain for user retrieved successfully", avgNeto);
+        return new ResponseMessage<>("Average cash gain for user retrieved successfully.", avgNeto);
     }
 
     @RequestMapping(method = GET, value="/statistics/{username}/avgGross")
     public ResponseMessage getUserAvgGrossProfit(@PathVariable("username") String userName) throws EntityDoesNotExistsException, InvalidArgumentException {
         double avgGross=service.getUserAvgGrossProfit(userName);
-        return new ResponseMessage<>("Average cash gain for user retrieved successfully", avgGross);
+        return new ResponseMessage<>("Average cash gain for user retrieved successfully.", avgGross);
     }
 
+    @RequestMapping(method = GET, value="/statistics/gamesPlayed")
+    public ResponseMessage getTop20UsersByNumOfGamesPlayed() {
+        List<Pair<String,Integer>> result = service.getTop20UsersByNumOfGamesPlayed();
+        return new ResponseMessage<>("Top 20 players by games played amount retrieved successfully.", result);
+    }
+
+    @RequestMapping(method = GET, value="/statistics/cashGain")
+    public ResponseMessage getTop20UsersByHighestCashGain() {
+        List<Pair<String,Integer>> result = service.getTop20UsersByHighestCashGain();
+        return new ResponseMessage<>("Top 20 players by cash gain retrieved successfully.", result);
+    }
+
+    @RequestMapping(method = GET, value="/statistics/grossProfit")
+    public ResponseMessage getTop20UsersByGrossProfit() {
+        List<Pair<String,Integer>> result = service.getTop20UsersByGrossProfit();
+        return new ResponseMessage<>("Top 20 players by gross profit retrieved successfully.", result);
+    }
 }
