c6e2c60dcaf3f091906a898ef5442390db558ad7
achiadg <achiadg@gmail.com>
2017-06-22 21:31:20 +0300

    all-in use case is supported.

--- src/test/java/Server/domain/game/RoundTest.java
+++ src/test/java/Server/domain/game/RoundTest.java
@@ -124,10 +124,10 @@ public class RoundTest {
 
     @Test
     public void round1_testPreFlop_AllCheck() throws Exception {
-        Player player1 = mock(Player.class);
-        Player player2 = mock(Player.class);
-        Player player3 = mock(Player.class);
-        Player player4 = mock(Player.class);
+        Player player1 = new Player(user1, 100, 100);
+        Player player2 = new Player(user2, 100, 100);
+        Player player3 = new Player(user3, 100, 100);
+        Player player4 = new Player(user4, 100, 100);
 
         List<Player> playerList1 = new LinkedList<Player>();
         playerList1.add(player1);
@@ -139,24 +139,29 @@ public class RoundTest {
         Game game1 = new Game(settings1, user1, new LeagueManager());
         Round round1 = new Round(playerList1, settings1, 0);
 
-        MoveEvent me1 = new MoveEvent(user1.getUsername(), GameActions.CHECK, 0, game1.getName());
-        MoveEvent me2 = new MoveEvent(user2.getUsername(), GameActions.CHECK, 0, game1.getName());
-        MoveEvent me3 = new MoveEvent(user3.getUsername(), GameActions.CHECK, 0, game1.getName());
-        MoveEvent me4 = new MoveEvent(user4.getUsername(), GameActions.CHECK, 0, game1.getName());
+        MoveEvent me1 = new MoveEvent(player1.getUser().getUsername(), GameActions.CHECK, 0, game1.getName());
+        MoveEvent me2 = new MoveEvent(player2.getUser().getUsername(), GameActions.CHECK, 0, game1.getName());
+        MoveEvent me3 = new MoveEvent(player3.getUser().getUsername(), GameActions.CHECK, 0, game1.getName());
+        MoveEvent me4 = new MoveEvent(player4.getUser().getUsername(), GameActions.CHECK, 0, game1.getName());
 
         round1.setRoundActive(true);
         round1.setCurrentState(RoundState.PREFLOP);
         round1.setChipsToCall(0);
 
-        when(player1.getUser()).thenReturn(user1);
-        when(player2.getUser()).thenReturn(user2);
-        when(player3.getUser()).thenReturn(user3);
-        when(player4.getUser()).thenReturn(user4);
+        int oldChipsAmount4 = player4.getChipsAmount();
+        int oldChipsAmount1 = player1.getChipsAmount();
+        int oldChipsAmount2 = player2.getChipsAmount();
+        int oldChipsAmount3 = player3.getChipsAmount();
 
         round1.playTurnOfPlayer(me4);
         round1.playTurnOfPlayer(me1);
         round1.playTurnOfPlayer(me2);
         round1.playTurnOfPlayer(me3);
+
+        Assert.assertEquals(oldChipsAmount4, player4.getChipsAmount());
+        Assert.assertEquals(oldChipsAmount1, player1.getChipsAmount());
+        Assert.assertEquals(oldChipsAmount2, player2.getChipsAmount());
+        Assert.assertEquals(oldChipsAmount3, player3.getChipsAmount());
 
         Assert.assertTrue(round1.getPotAmount() == 0);
         Assert.assertTrue(round1.getActivePlayers().size() == 4);
@@ -166,10 +171,10 @@ public class RoundTest {
 
     @Test
     public void round1_testPreFlop_MixOfActions() throws Exception {
-        Player player1 = mock(Player.class);
-        Player player2 = mock(Player.class);
-        Player player3 = mock(Player.class);
-        Player player4 = mock(Player.class);
+        Player player1 = new Player(user1, 100, 100);
+        Player player2 = new Player(user2, 100, 100);
+        Player player3 = new Player(user3, 100, 100);
+        Player player4 = new Player(user4, 100, 100);
 
         List<Player> playerList1 = new LinkedList<Player>();
         playerList1.add(player1);
@@ -185,37 +190,34 @@ public class RoundTest {
         round1.setCurrentState(RoundState.TURN);
 
         // Player 4 is the first player
-        MoveEvent me4 = new MoveEvent(user4.getUsername(), GameActions.CALL, 0, game1.getName());
-        MoveEvent me1 = new MoveEvent(user1.getUsername(), GameActions.FOLD, 0, game1.getName());
-        MoveEvent me2 = new MoveEvent(user2.getUsername(), GameActions.CALL, 0, game1.getName());
-        MoveEvent me3 = new MoveEvent(user3.getUsername(), GameActions.RAISE, 20, game1.getName());
-        MoveEvent me41 = new MoveEvent(user4.getUsername(), GameActions.CALL, 0, game1.getName());
-        MoveEvent me21 = new MoveEvent(user2.getUsername(), GameActions.FOLD, 0, game1.getName());
+        MoveEvent me4 = new MoveEvent(player4.getUser().getUsername(), GameActions.CALL, 0, game1.getName());
+        MoveEvent me1 = new MoveEvent(player1.getUser().getUsername(), GameActions.FOLD, 0, game1.getName());
+        MoveEvent me2 = new MoveEvent(player2.getUser().getUsername(), GameActions.CALL, 0, game1.getName());
+        MoveEvent me3 = new MoveEvent(player3.getUser().getUsername(), GameActions.RAISE, 20, game1.getName());
+        MoveEvent me41 = new MoveEvent(player4.getUser().getUsername(), GameActions.CALL, 0, game1.getName());
+        MoveEvent me21 = new MoveEvent(player2.getUser().getUsername(), GameActions.FOLD, 0, game1.getName());
 
-        when(player1.getUser()).thenReturn(user1);
-        when(player2.getUser()).thenReturn(user2);
-        when(player3.getUser()).thenReturn(user3);
-        when(player3.getChipsAmount()).thenReturn(90);
-        when(player4.getUser()).thenReturn(user4);
-        when(player4.payChips(10)).thenReturn(10).thenReturn(10);
-        when(player4.getLastBetSinceCardOpen()).thenReturn(0).thenReturn(0).thenReturn(10).thenReturn(10);
-        when(player2.payChips(10)).thenReturn(10);
-        when(player3.payChips(20)).thenReturn(20);
+        int oldChipsAmount4 = player4.getChipsAmount();
+        int oldChipsAmount1 = player1.getChipsAmount();
+        int oldChipsAmount2 = player2.getChipsAmount();
+        int oldChipsAmount3 = player3.getChipsAmount();
 
         round1.playTurnOfPlayer(me4);
+        Assert.assertEquals(oldChipsAmount4 - 10, player4.getChipsAmount());
         round1.playTurnOfPlayer(me1);
+        Assert.assertEquals(oldChipsAmount1, player1.getChipsAmount());
         round1.playTurnOfPlayer(me2);
+        Assert.assertEquals(oldChipsAmount2 - 10, player2.getChipsAmount());
         round1.playTurnOfPlayer(me3);
+        Assert.assertEquals(oldChipsAmount3 - 20, player3.getChipsAmount());
         round1.playTurnOfPlayer(me41);
+        Assert.assertEquals(oldChipsAmount4 - 20, player4.getChipsAmount());
         round1.playTurnOfPlayer(me21);
+        Assert.assertEquals(oldChipsAmount2 - 10, player2.getChipsAmount());
 
         Assert.assertTrue(round1.getPotAmount() == 50);
         Assert.assertTrue(round1.getActivePlayers().size() == 2);
         Assert.assertTrue(round1.isRoundActive() == true);
-
-        verify(player4, times(2)).payChips(10);
-        verify(player3).payChips(20);
-        verify(player2).payChips(10);
     }
 
     @Test
