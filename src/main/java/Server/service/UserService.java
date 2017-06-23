79a2ff06e1437b6abdbbc0feceefacf55c4bec30
TalBarami <talbaramii@gmail.com>
2017-06-23 16:39:25 +0300

    Added logs for session use cases.

--- src/main/java/Server/service/UserService.java
+++ src/main/java/Server/service/UserService.java
@@ -6,6 +6,8 @@ import Exceptions.InvalidArgumentException;
 import Exceptions.LoginException;
 import Server.domain.system.GameCenter;
 import Server.domain.user.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.awt.image.BufferedImage;
 import java.time.LocalDate;
@@ -20,6 +22,8 @@ import static Server.service.TexasHoldemService.verifyStrings;
  * Created by Tal on 05/05/2017.
  */
 public class UserService {
+    private static Logger logger = LoggerFactory.getLogger(UserService.class);
+
     private GameCenter gameCenter;
 
     public UserService(GameCenter gameCenter){
@@ -27,33 +31,39 @@ public class UserService {
     }
 
     public void register(String username, String pass, String email, LocalDate date, BufferedImage img) throws InvalidArgumentException {
+        logger.info("Received registration request: username={}, pass={}, email={}, date={}, img={}", username, pass, email, date, img);
         verifyStrings(username, pass, email);
         verifyObjects(date);
         gameCenter.registerUser(username, pass, email, date, img);
     }
 
     public void deleteUser(String username) throws EntityDoesNotExistsException, InvalidArgumentException {
+        logger.info("Received deletion request: username={}", username);
         verifyStrings(username);
         gameCenter.deleteUser(username);
     }
 
     public void login(String username,String pass) throws LoginException, InvalidArgumentException, EntityDoesNotExistsException {
+        logger.info("Received login request: username={}, pass={}", username, pass);
         verifyStrings(username);
         gameCenter.login(username, pass);
     }
 
     public void logout(String username) throws InvalidArgumentException {
+        logger.info("Received logout request: username={}", username);
         verifyStrings(username);
         gameCenter.logout(username);
     }
 
     public void editProfile(String originalUserName,String newUserName, String pass,String email, LocalDate date) throws InvalidArgumentException, EntityDoesNotExistsException {
+        logger.info("Received edit profile request: original usernamme={}, new username={}, pass={}, email={}, date={}", originalUserName, newUserName, pass, email, date);
         verifyStrings(originalUserName, newUserName, pass, email);
         verifyObjects(date);
         gameCenter.editProfile(originalUserName, newUserName, pass, email, date);
     }
 
     public void deposit(String username, int amount) throws ArgumentNotInBoundsException, InvalidArgumentException, EntityDoesNotExistsException {
+        logger.info("Received deposit request: original usernamme={}, amount={}", username, amount);
         verifyStrings(username);
         verifyPositiveNumbers(amount);
         gameCenter.depositMoney(username, amount);
